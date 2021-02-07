d50dcc2467a9d2fd7977fff64bc7221d771b4aa9
MayamaTakeshi <mayamatakeshi@gmail.com>
2021-02-07 11:27:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# shout_to_gsr (shoutcast to Google Speech Recognition)
+
+## Overview
+
+This is a node.js showing how to use freeswitch/mod_shout record_session to convert speech to text using Google Speech Recognition
+This is a simple shoutcast server application that will send the audio received to Google Speech Recogntion service and print the result to the shell.
+
+## Configuration
+
+In your freeswitch dialplan, you will need an extension with actions like these (adjust SHOUT_TO_GSR_IP_ADDRESS)
+```
+  <action application='set' data='enable_file_write_buffering=false'/>
+  <action application='answer'/>
+  <action application='record_session' data='shout://user:pass@SHOUT_TO_GSR_IP_ADDRESS:9999/speech_recog?uuid=${uuid}'/>
+  <action application='park'/>
+```
+
+You will also need to obtain an Google application credentials file with Speech Recognition (Speech-To-Text) support enabled.
+
+## Testing
+
+Start the server:
+
+```
+npm install
+export GOOGLE_APPLICATION_CREDENTIALS=/path/to/your/credentials_file.json
+node simple.js
+```
+
+Then make a call to reach the above dialplan and whatever you say will be converted to text and output to the shell.
+
+
+
---
+++ simple.js
@@ -0,0 +1,107 @@
+const net = require('net')
+var httpHeaders = require('http-headers')
+
+const speech = require('@google-cloud/speech')
+
+const lame = require('lame')
+
+const speech_client = new speech.SpeechClient()
+
+const m = require('moment')
+
+var log = function(msg) {
+    const now = m().format("YYYY-MM-DD HH:mm:ss")
+    console.log(`${now}: ${msg}`)
+}
+
+var start_speech_recog = function(uuid, socket, initial_data) {
+    const encoding = 'LINEAR16'
+    const sampleRateHertz = 8000
+    const languageCode = 'en-US'
+
+    const request = {
+        config: {
+            encoding: encoding,
+            sampleRateHertz: sampleRateHertz,
+            languageCode: languageCode,
+        },
+        interimResults: true, // If you want interim results, set this to true
+    };
+
+    // Stream the audio to the Google Cloud Speech API
+    var recognizeStream = speech_client
+    .streamingRecognize(request)
+    .on('error', error => {
+        log(`${uuid} recognizeStream error ${error}`)
+    })
+    .on('data', data => {
+        log(`Transcription: ${data.results[0].alternatives[0].transcript}`)
+    })
+    .on('close', () => {
+        log(`${uuid} recognizeStream close`)
+    })
+    
+    var decoder = new lame.Decoder();
+
+    decoder.on('format', (format) => {
+        log(`${uuid} MP3 format: ${JSON.stringify(format)}`)
+
+        decoder.pipe(recognizeStream)
+    })
+
+    //decoder.on('data', data => log(`${uuid} MP3 data`))
+
+    if(initial_data != "") {
+        decoder.write(Buffer.from(initial_data))
+    }
+
+    socket.pipe(decoder)
+
+    //socket.on('data', data => log(`${uuid} socket data length=${data.length}`))
+
+    socket.on('close', () => {
+        log(`${uuid} socket close`)
+        decoder.unpipe(recognizeStream)
+        recognizeStream.end()
+    })
+
+    socket.on('error', () => {
+        log(`${uuid} socket error ${error}`)
+        decoder.unpipe(recognizeStream)
+        recognizeStream.end()
+    })
+}
+
+
+const server = net.createServer()
+server.on('connection', socket => {
+    log('new client arrived')
+
+    var acc = ""
+
+    var handshake = function(data) {
+        acc = acc + data.toString()
+        var idx = acc.indexOf("\r\n\r\n")
+        if(idx < 0) {
+            socket.once('data', handshake)
+            return
+        }
+
+        var msg = acc.slice(0, idx)
+        var headers = httpHeaders(msg)
+        console.dir(headers)
+        socket.write("HTTP/1.1 200 OK\r\n\r\n")
+
+        var uuid = headers.url.split("=")[1]
+        var rest = acc.slice(idx+4)
+
+        start_speech_recog(uuid, socket, rest)
+    } 
+ 
+    socket.once('data', handshake)
+})
+
+const PORT = 9999
+
+server.listen(PORT, () => { log(`Listening on port ${PORT}`) })
+
